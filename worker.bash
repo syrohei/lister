a357fa8f453580f742adbfd2a445dd57e19e8fe0
syrohei <syrohei@gmail.com>
2017-01-15 03:24:13 +0000

    check some point

--- worker.bash
+++ worker.bash
@@ -3,3 +3,4 @@
 
 AWS_REGION=us-east-1 TICKER_TABLE=tickers forever start worker/btcbox.js
 AWS_REGION=us-east-1 TICKER_TABLE=tickers forever start worker/coincheck.js
+AWS_REGION=us-east-1 TICKER_TABLE=tickers forever start worker/quoine.js
---
+++ worker/quoine.js
@@ -0,0 +1,74 @@
+const ticker = require('../models/ticker.js');
+const btcbox = require('../lib/exchanges/btcbox.js')
+const coincheck = require('../lib/exchanges/coincheck.js')
+const quoine = require('../lib/exchanges/quoine.js')
+
+const moment = require('moment')
+const _ = require('lodash');
+
+const mail = require('../models/mail.js')
+
+var ask, bid
+
+const ex = {
+  'btcbox': btcbox,
+  'coincheck': coincheck,
+  'quoine': quoine
+}
+
+const RecreateData = (exchange_name, term, ask, tickers) => {
+  if (tickers.Count > 0) {
+    const updateItem = _.sortBy(tickers.Items, 'timestamp').pop()
+    updateItem.close = ask
+    if (updateItem.high < ask) {
+      updateItem.high = ask
+    }
+    if (updateItem.low > ask) {
+      updateItem.low = ask
+    }
+    console.log("update", updateItem.timestamp);
+    return updateItem
+  } else {
+    const now = moment().unix()
+    const _exchange_name = exchange_name + "_" + term + "s"
+    const newItem = {
+      exchange_name: _exchange_name,
+      term: term,
+      timestamp: String(now),
+      high: String(ask),
+      low: String(ask),
+      open: String(ask),
+      close: String(ask)
+    }
+    console.log("newcreate", newItem.timestamp);
+
+    return newItem
+  }
+}
+
+const exchange_worker = (exchange_name, term) => {
+  ex[exchange_name].getAsk().then((ask) => {
+    ticker.findTicker({
+      exchange_name: exchange_name,
+      term: term
+    }).then((tickers) => {
+      const data = RecreateData(exchange_name, term, ask.toFixed(), tickers)
+      console.log(data);
+
+      return ticker.updateTicker(data)
+    }).then((result) => {
+      setTimeout(() => {
+        exchange_worker(exchange_name, term)
+      }, 1500)
+    }).catch((err) => {
+      console.log(err);
+      setTimeout(() => {
+        exchange_worker(exchange_name, term)
+      }, 1500)
+    })
+  })
+}
+
+
+
+exchange_worker('quoine', '60')

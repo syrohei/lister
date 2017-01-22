8a7a137823bf59c4c98fbe0d6ff34b00331ef695
syrohei <syrohei@gmail.com>
2017-01-22 19:12:04 +0900

    check release 1

--- worker.bash
+++ worker.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 
-AWS_REGION=us-east-1 TICKER_TABLE=tickers forever start worker/btcbox.js
-AWS_REGION=us-east-1 TICKER_TABLE=tickers forever start worker/coincheck.js
-AWS_REGION=us-east-1 TICKER_TABLE=tickers forever start worker/quoine.js
+AWS_REGION=ap-northeast-1 TICKER_TABLE=tickers forever start worker/btcbox.js
+AWS_REGION=ap-northeast-1 TICKER_TABLE=tickers forever start worker/coincheck.js
+AWS_REGION=ap-northeast-1 TICKER_TABLE=tickers forever start worker/quoine.js
